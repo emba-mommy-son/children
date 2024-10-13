c6f4f7d29fba53689805e962e1cb8abe25c91b0c
김도영 <rlaehdud1002@naver.com>
2024-10-13 12:05:13 +0900

    [FEAT] bottom sheet

--- src/pages/friend/components/FriendItem.tsx
+++ src/pages/friend/components/FriendItem.tsx
@@ -1,15 +1,16 @@
 // 리액트
-import {useState} from 'react';
+import {useCallback, useMemo, useRef, useState} from 'react';
 import {Image, Text, TouchableOpacity, View} from 'react-native';
 
 // 타입
 import {FriendResponse} from 'types/friend';
 
 // 컴포넌트
-import {Drawer} from '@components/ui/Drawer';
 
 // 아이콘
 import Friend from '@assets/icons/friend/friendImage.png';
+import {BottomSheetModal, BottomSheetModalProvider} from '@gorhom/bottom-sheet';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 export const FriendItem: React.FC<{item: FriendResponse}> = ({item}) => {
   const [open, setOpen] = useState<boolean>(false);
@@ -18,23 +19,41 @@ export const FriendItem: React.FC<{item: FriendResponse}> = ({item}) => {
     setOpen(!open);
   };
 
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  const snapPoints = useMemo(() => ['25%', '50%'], []);
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
   return (
-    <View>
-      <TouchableOpacity onPress={handleOpen}>
-        <View className="flex flex-row items-center py-5 border-b-2 border-gray-700 space-x-6">
-          <Image source={Friend} className="w-16 h-16" />
-          <Text className="text-body-text text-black font-bold">
-            {item.name}
-          </Text>
+    <GestureHandlerRootView>
+      <BottomSheetModalProvider>
+        <View>
+          <TouchableOpacity onPress={handlePresentModalPress}>
+            <View className="flex flex-row items-center py-5 border-b-2 border-gray-700 space-x-6">
+              <Image source={Friend} className="w-16 h-16" />
+              <Text className="text-body-text text-black font-bold">
+                {item.name}
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <BottomSheetModal
+            ref={bottomSheetModalRef}
+            index={1}
+            snapPoints={snapPoints}
+            onChange={handleSheetChanges}>
+            <View>
+              <Text>이거지</Text>
+            </View>
+          </BottomSheetModal>
         </View>
-      </TouchableOpacity>
-      {open && (
-        <Drawer>
-          <View>
-            <Text>이거지</Text>
-          </View>
-        </Drawer>
-      )}
-    </View>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 };
--- src/pages/friend/pages/FriendPage.tsx
+++ src/pages/friend/pages/FriendPage.tsx
@@ -9,22 +9,25 @@ import {ErrorComponent} from '@components/common/ErrorComponent';
 import {FriendList} from '@pages/friend/components/FriendList';
 import ErrorBoundary from 'react-native-error-boundary';
 import AntDesignIcons from 'react-native-vector-icons/AntDesign';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export const FriendPage = () => {
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <View className="bg-secondary p-4 flex-row items-center justify-between">
-        <Text className="text-white text-subheading font-semibold">친구</Text>
-        <View className="flex flex-row space-x-3">
-          <AntDesignIcons name="search1" color="white" size={25} />
-          <AntDesignIcons name="adduser" color="white" size={25} />
+    <GestureHandlerRootView>
+      <SafeAreaView className="flex-1 bg-white">
+        <View className="bg-secondary p-4 flex-row items-center justify-between">
+          <Text className="text-white text-subheading font-semibold">친구</Text>
+          <View className="flex flex-row space-x-3">
+            <AntDesignIcons name="search1" color="white" size={25} />
+            <AntDesignIcons name="adduser" color="white" size={25} />
+          </View>
         </View>
-      </View>
-      <ErrorBoundary FallbackComponent={ErrorComponent}>
-        <Suspense fallback={<ActivityIndicator color="#9D4BFF" />}>
-          <FriendList />
-        </Suspense>
-      </ErrorBoundary>
-    </SafeAreaView>
+        <ErrorBoundary FallbackComponent={ErrorComponent}>
+          <Suspense fallback={<ActivityIndicator color="#9D4BFF" />}>
+            <FriendList />
+          </Suspense>
+        </ErrorBoundary>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
