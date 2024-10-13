cc293c5e955f0e9f23edb8d328168f8fc22e907e
junhee <limjunhee4575@gmail.com>
2024-10-13 15:58:47 +0900

    feat: 소켓연결

--- index.js
+++ index.js
@@ -5,5 +5,24 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import websocket from 'websocket';
+import {WebSocket as WSWebSocket} from 'ws';
+import * as encoding from 'text-encoding';
 
+// WebSocket 설정
+if (!global.WebSocket) {
+  if (websocket && websocket.w3cwebsocket) {
+    global.WebSocket = websocket.w3cwebsocket;
+  } else if (WSWebSocket) {
+    global.WebSocket = WSWebSocket;
+  }
+}
+
+// TextEncoder/TextDecoder 설정
+if (typeof TextEncoder === 'undefined') {
+  global.TextEncoder = encoding.TextEncoder;
+}
+if (typeof TextDecoder === 'undefined') {
+  global.TextDecoder = encoding.TextDecoder;
+}
 AppRegistry.registerComponent(appName, () => App);
