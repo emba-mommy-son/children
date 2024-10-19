a06d10587335cec8306052bc839b748d8585568c
김도영 <rlaehdud1002@naver.com>
2024-10-19 18:08:42 +0900

    [FEAT] 폰번호로 친구 조회 연결

--- src/pages/friend/components/AddFriendResult.tsx
+++ src/pages/friend/components/AddFriendResult.tsx
@@ -1,13 +1,35 @@
-import FriendImage from '@/assets/icons/friend/friendImage.png';
+import {UserInfo} from '@/types/user';
 import {Image, Text, View} from 'react-native';
 
-export const AddFriendResult = () => {
+interface AddFriendResultProps {
+  friendData: UserInfo;
+  setFriendData: (friendData: UserInfo | null) => void;
+  reset: () => void;
+}
+
+export const AddFriendResult = ({
+  friendData,
+  setFriendData,
+  reset,
+}: AddFriendResultProps) => {
+  const handleClear = () => {
+    reset();
+    setFriendData(null);
+  };
+  
   return (
-    <View className="bg-primary/20 m-4 p-5 rounded-xl flex flex-col items-center justify-center space-y-6">
-      <Image source={FriendImage} className="w-24 h-24" />
-      <Text className="text-subheading text-black font-bold">김민지</Text>
+    <View className="bg-primary/20 m-4 p-5 rounded-xl flex flex-col items-center justify-center space-y-4">
+      <Image
+        source={{uri: friendData.profileImage}}
+        className="w-20 h-20 rounded-full"
+      />
+      <Text className="text-subheading text-black font-bold">
+        {friendData.name}
+      </Text>
       <View className="flex flex-row space-x-5">
-        <Text className="bg-white text-body-text text-black font-bold rounded-xl px-10 py-2">
+        <Text
+          className="bg-white text-body-text text-black font-bold rounded-xl px-10 py-2"
+          onPress={handleClear}>
           취소
         </Text>
         <Text className="bg-primary text-body-text text-white font-bold rounded-xl px-10 py-2">
