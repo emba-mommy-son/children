3e3fa0fb50c7469d25186f65f12ee429efd13d51
junhee <limjunhee4575@gmail.com>
2024-10-19 17:55:40 +0900

    feat : 네비게이션 상수 분리

---
+++ src/constants/routes.ts
@@ -0,0 +1,16 @@
+export const ROUTES = {
+  // Tab Routes
+  MAIN_TABS: 'MainTabs',
+  HOME: 'Home',
+  FRIEND: 'Friend',
+  CHATTING_LIST: 'ChattingList',
+  SLEEP: 'Sleep',
+  LOCATION: 'Location',
+
+  // Stack Routes
+  ALARM: 'Alarm',
+  REWARD: 'Reward',
+  CHATTING: 'Chatting',
+  FRIEND_RANKING: 'FriendRanking',
+  ADD_FRIEND: 'AddFriend',
+} as const;
