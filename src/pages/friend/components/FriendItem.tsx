3c28e9423cca1f59fbbe1f989a306c23fbfbf359
junhee <limjunhee4575@gmail.com>
2024-10-20 13:48:29 +0900

    feat : 친구 추가, 삭제 기능

--- src/pages/friend/components/FriendItem.tsx
+++ src/pages/friend/components/FriendItem.tsx
@@ -1,32 +1,19 @@
 // 리액트
-import {Image, Text, TouchableOpacity, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 
 // 타입
 import {Friend} from '@/types/friend';
 
 export const FriendItem: React.FC<{item: Friend}> = ({item}) => {
   return (
-    <View>
-      <TouchableOpacity>
-        <View className="flex flex-row items-center py-5 border-b-2 border-gray-700 space-x-6">
-          <Image
-            source={{uri: item.profileImage}}
-            className="w-16 h-16 rounded-full"
-          />
-          <Text className="text-body-text text-black font-bold">
-            {item.name}
-          </Text>
-        </View>
-      </TouchableOpacity>
-      {/* <BottomSheetModal
-        ref={bottomSheetModalRef}
-        index={1}
-        snapPoints={snapPoints}
-        onChange={handleSheetChanges}>
-        <View>
-          <Text>이거지</Text>
-        </View>
-      </BottomSheetModal> */}
+    <View className="bg-white flex flex-row items-center py-5 border-b-[1px] border-gray-200 space-x-6">
+      <Image
+        source={{uri: item.profileImage}}
+        className="w-16 h-16 rounded-full"
+      />
+      <Text className="text-body-text text-black font-bold flex-1">
+        {item.name}
+      </Text>
     </View>
   );
 };
