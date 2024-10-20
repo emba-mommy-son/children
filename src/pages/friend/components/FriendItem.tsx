8b4c87f844254ae6b4fb517849f783449fafb832
junhee <limjunhee4575@gmail.com>
2024-10-20 15:35:29 +0900

    feat : 채팅방 생성, 친구 삭제 기능

--- src/pages/friend/components/FriendItem.tsx
+++ src/pages/friend/components/FriendItem.tsx
@@ -1,19 +1,35 @@
-// 리액트
-import {Image, Text, View} from 'react-native';
-
-// 타입
+import React from 'react';
+import {
+  Image,
+  Text,
+  View,
+  TouchableOpacity,
+  GestureResponderEvent,
+} from 'react-native';
 import {Friend} from '@/types/friend';
 
-export const FriendItem: React.FC<{item: Friend}> = ({item}) => {
+interface FriendItemProps {
+  item: Friend;
+  onPress: () => void;
+}
+
+export const FriendItem: React.FC<FriendItemProps> = ({item, onPress}) => {
+  const handlePress = (event: GestureResponderEvent) => {
+    event.stopPropagation();
+    onPress();
+  };
+
   return (
-    <View className="bg-white flex flex-row items-center py-5 border-b-[1px] border-gray-200 space-x-6">
-      <Image
-        source={{uri: item.profileImage}}
-        className="w-16 h-16 rounded-full"
-      />
-      <Text className="text-body-text text-black font-bold flex-1">
-        {item.name}
-      </Text>
-    </View>
+    <TouchableOpacity onPress={handlePress} activeOpacity={1}>
+      <View className="bg-white flex flex-row items-center py-5 border-b-[1px] border-gray-200 space-x-6">
+        <Image
+          source={{uri: item.profileImage}}
+          className="w-16 h-16 rounded-full"
+        />
+        <Text className="text-body-text text-black font-bold flex-1">
+          {item.name}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
