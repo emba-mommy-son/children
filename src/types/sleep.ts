b8eb1cd9ac72f35266332809da32892e0ced37ef
junhee <limjunhee4575@gmail.com>
2024-10-23 23:33:37 +0900

    feat: 수면데이터 가져오는 훅 생성

---
+++ src/types/sleep.ts
@@ -0,0 +1,14 @@
+export interface SleepData {
+  id: string;
+  startDate: Date;
+  endDate: Date;
+  value: string; // INBED | ASLEEP | AWAKE 일수도?
+  isSynced: boolean;
+  syncAttempts: number;
+  lastSyncAttempt?: Date;
+}
+
+export interface HealthServiceError {
+  code: string;
+  message: string;
+}
