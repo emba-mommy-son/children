a70df7fc2008548cb3bda8cee1e2cb965050cbe4
junhee <limjunhee4575@gmail.com>
2024-11-09 23:29:19 +0900

    design : 수면 페이지 디자인 수정

--- src/pages/sleep/components/StatCard.tsx
+++ src/pages/sleep/components/StatCard.tsx
@@ -41,8 +41,8 @@ export const StatCard: React.FC<StatCardProps> = ({title, value, unit}) => {
         </View>
       ) : (
         // 평균 수면시간 표시
-        <View className="flex-row items-center justify-center text-center">
-          <Text className="text-white text-4xl font-bold">
+        <View className="flex-row items-center justify-center text-center p-4">
+          <Text className="text-white text-xl font-bold">
             {formatValue(value)}
           </Text>
           <Text className="text-white text-lg ml-1">{unit}</Text>
